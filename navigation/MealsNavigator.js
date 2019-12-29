cc630ebc4e182383d5448be6727652ac6c001313
dannyseymour <daniel@discordia.io>
2019-12-28 17:46:32 -0700

    tab bar navigation

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -6,6 +6,10 @@ import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
 import {Platform} from "react-native";
 import Colors from "../constants/Colors";
+import { createBottomTabNavigator} from "react-navigation-tabs";
+import FavoritesScreen from "../screens/FavoritesScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
@@ -23,8 +27,42 @@ const MealsNavigator = createStackNavigator({
       headerTintColor: Platform.OS ==='android' ? 'white' : Colors.primaryColor
     }}
     );
+const tabScreenConfig = {
+      MealsStack: {
+        screen: MealsNavigator,
+        navigationOptions: {
+          tabBarLabel: 'Meals',
+          tabBarIcon: (tabInfo) =>{
+            return <Ionicons name={'ios-restaurant'} size={25} color={tabInfo.tintColor}/>
+          }
+        },
+        tabBarColor: Colors.primaryColor,
+      },
+      Favorites: {
+        screen: FavoritesScreen,
+        navigationOptions:{
+          tabBarIcon: (tabInfo) =>{
+            return <Ionicons name={'ios-star'} size={25} color={tabInfo.tintColor}/>
+          }
+        },
+        tabBarColor: Colors.accentColor,
+      },
 
-export default createAppContainer(MealsNavigator);
+    };
+
+
+const MealsFavTabNavigator =  Platform.OS === 'android' ? createMaterialBottomTabNavigator(
+    tabScreenConfig,
+    {
+      activeTintColor: 'white',
+      shifting: true
+    }
+) : createBottomTabNavigator( tabScreenConfig,
+    {
+      tabBarOptions:{activeTintColor: Colors.accentColor}
+    });
+
+export default createAppContainer(MealsFavTabNavigator);
 
 
 
